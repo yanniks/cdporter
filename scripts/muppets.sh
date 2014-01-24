3674ea2449eb2eb2e0b50b9e5f3a68eefed6a81f
yanniks <kontakt@yanniks.de>
2014-01-24 17:45:06 +0100

    fix TheMuppets vendor

--- scripts/muppets.sh
+++ scripts/muppets.sh
@@ -1,33 +1,33 @@
 #/bin/bash
 
 echo "adding TheMuppets vendor..."
-if [ -d "an*samsung*$2" ];then
+if [ -d an*samsung*$2 ];then
 	export VENDOR=samsung
-elif [ -d "an*htc*$2" ];then
+elif [ -d an*htc*$2 ];then
 	export VENDOR=htc
-elif [ -d "an*motorola*$2" ];then
+elif [ -d an*motorola*$2 ];then
 	export VENDOR=motorola
-elif [ -d "an*bn*$2" ];then
+elif [ -d an*bn*$2 ];then
 	export VENDOR=bn
-elif [ -d "an*sony*$2" ];then
+elif [ -d an*sony*$2 ];then
 	export VENDOR=sony
-elif [ -d "an*acer*$2" ];then
+elif [ -d an*acer*$2 ];then
 	export VENDOR=acer
-elif [ -d "an*lge*$2" ];then
+elif [ -d an*lge*$2 ];then
 	export VENDOR=lge
-elif [ -d "an*oppo*$2" ];then
+elif [ -d an*oppo*$2 ];then
 	export VENDOR=oppo
-elif [ -d "an*nvidia*$2" ];then
+elif [ -d an*nvidia*$2 ];then
 	export VENDOR=nvidia
-elif [ -d "an*amazon*$2" ];then
+elif [ -d an*amazon*$2 ];then
 	export VENDOR=amazon
-elif [ -d "an*huawei*$2" ];then
+elif [ -d an*huawei*$2 ];then
 	export VENDOR=huawei
-elif [ -d "an*hp*$2" ];then
+elif [ -d an*hp*$2 ];then
 	export VENDOR=hp
-elif [ -d "an*zte*$2" ];then
+elif [ -d an*zte*$2 ];then
 	export VENDOR=zte
-elif [ -d "an*google*$2" ];then
+elif [ -d an*google*$2 ];then
 	export VENDOR=google
 fi
 
