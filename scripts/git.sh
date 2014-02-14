0b3c3475aee5228e953cc313a1af8778e8cde1e7
yanniks <kontakt@yanniks.de>
2014-02-14 21:43:06 +0100

    commit vendorsetup.sh too

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,7 +5,7 @@ cd $1
 echo "creating git commit..."
 git branch cd-4.4 > /dev/null
 git checkout cd-4.4 > /dev/null
-git add cd.dependencies cd.mk > /dev/null
+git add vendorsetup.sh cd.dependencies cd.mk > /dev/null
 git rm cm.mk cm.dependencies > /dev/null
 git commit -m "initial CyanDream commit" > /dev/null
 echo "git stuff done! Please push the code to your new device repository."
