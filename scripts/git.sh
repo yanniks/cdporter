39511b01d78de94d84164df3c856ce2dcbd03b59
yanniks <kontakt@yanniks.de>
2014-02-28 21:29:38 +0100

    fix git commits if no vendorsetup.sh file is present
    Change-Id: Ica730e9674e5027d96f41354f3034ce8d197ef6d

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-sleep 2
 export current=$PWD
 cd $1
 echo "creating git commit..."
 git branch cd-4.4 > /dev/null
 git checkout cd-4.4 > /dev/null
-git add vendorsetup.sh cd.* > /dev/null
+git add cd.* > /dev/null
+git add vendorsetup.sh > /dev/null
 git rm cm.mk cm.dependencies > /dev/null
 git commit -m "initial CyanDream commit" > /dev/null
 echo "git stuff done! Please push the code to your new device repository."
